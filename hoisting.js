d7b85e190fa73a50e5398aca6658b0efa7cda9a0
James Stamps Jr <jstampshere@gmail.com>
2020-01-28 00:00:07 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -8,15 +8,16 @@ function callMe() {
 
 }
 
-function thisIsCrazy(){
-  console.log ("hey!!!")
-}
+
 function crazy() {
   // fix the code in here:
 
 
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
   }
-
+thisIsCrazy();
+}
 
 function sayMyName() {
   // fix the code in here:
